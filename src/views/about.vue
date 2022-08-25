e51330fb464656c903265c77c85ecd30cb64412b
Sakura404 <380487608@qq.com>
2022-08-25 19:12:50 +0800

    关于

--- src/views/about.vue
+++ src/views/about.vue
@@ -4,9 +4,9 @@
     <hr />
     <h2>前端</h2>
     <p>mvvc框架Vue ,组件库Vuetify ,文本编辑器Tinymce,</p>
+    <p>nuxt.实现ssr，github action 实现自动部署</p>
     <h2>后端</h2>
     <p>SpringBoot,mybatis,jsonwebtoken</p>
-    <p>nuxt.实现ssr，github action 实现自动部署</p>
   </home-template>
 </template>
 <script>
